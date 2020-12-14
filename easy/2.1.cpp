faabff0572b51349588cdd7b8f2837b851cfabb7
xing393939 <xing393939@users.noreply.github.com>
2020-12-14 16:45:49 +0800

    add

--- easy/2.1.cpp
+++ easy/2.1.cpp
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-struct{
+struct {
     char c;
     int i[2];
     double v;
-}A;
+} A;
+
+void echo() {
+    char buf[4]; // 太小
+    scanf("%s", buf);
+    puts(buf);
+}
 
 int main(void) {
-    printf("the size of struct A：%d",sizeof(A));
+    echo();
+    return 0;
+    //printf("the size of struct A：%d",sizeof(A));
 }
