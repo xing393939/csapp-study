dc5d02f6ac5168776821ed7800e8eaefcbe32043
chenxing <chenxing@bookan.com.cn>
2021-01-04 11:11:30 +0800

    1

--- easy/1.1.cpp
+++ easy/1.1.cpp
@@ -10,11 +10,15 @@ void show_bytes(pointer start, size_t len) {
 }
 
 int main(void) {
-    /**
-     * 检测机器是不是小端规则
-     * 存储是从低地址->高地址，所以是小端规则
-     */
     char a[5] = "0123";
     printf("char a = %p\n", &a);
     show_bytes((pointer) &a, 4);
+
+    /**
+     * 检测机器是不是小端规则（需要用多字节的数据来验证，不能用char）
+     * 小端尾规则=尾部是低地址
+     */
+    int b = 15213; //16进制是00 00 3b 6d
+    printf("int b = 15213;\n");
+    show_bytes((pointer) &b, sizeof(int));
 }
