e719e91f1c061f1506521c7440e39c90553b2bbd
xing393939 <xing393939@users.noreply.github.com>
2021-03-24 15:25:19 +0800

    1

--- easy/test_rio.c
+++ easy/test_rio.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv) {
     rio_t rio;
     char buf[MAXLINE];
 
-    int fd = Open("test.txt", O_RDONLY, NULL);
+    int fd = Open("cmake_install.cmake", O_RDONLY, NULL);
     Rio_readinitb(&rio, fd);
 
     n = Rio_readlineb(&rio, buf, MAXLINE);
@@ -16,12 +16,6 @@ int main(int argc, char **argv) {
 
     n = Rio_readlineb(&rio, buf, MAXLINE);
     Rio_writen(STDOUT_FILENO, buf, n);
-
-    fd = Open("test.txt", O_RDONLY, NULL);
-    Read(fd, buf, 5);
-    Rio_writen(STDOUT_FILENO, buf, 5);
-    Read(fd, buf, 6);
-    Rio_writen(STDOUT_FILENO, buf, 6);
 
     exit(0);
 }
