6c6cd4728cf39d20f571175fc0f8e16f35b645c8
xing393939 <xing393939@users.noreply.github.com>
2022-02-27 14:00:50 +0800

    1

--- easy/echo_client.c
+++ easy/echo_client.c
@@ -5,21 +5,27 @@ int main(int argc, char **argv) {
     char *host, *port, buf[MAXLINE];
     rio_t rio;
 
-    host = "127.0.0.1";
-    port = "12102";
+    host = "localhost";
+    port = "80";
 
     // 建立连接（前面已经详细介绍）
     clientfd = Open_clientfd(host, atoi(port));
     Rio_readinitb(&rio, clientfd);
 
-    while (Fgets(buf, MAXLINE, stdin) != NULL) {
-        // 写入，也就是向服务器发送信息
-        Rio_writen(clientfd, buf, strlen(buf));
-        // 读取，也就是从服务器接收信息
-        Rio_readlineb(&rio, buf, MAXLINE);
-        // 把从服务器接收的信息显示在输出中
-        Fputs(buf, stdout);
-    }
+    //while (Fgets(buf, MAXLINE, stdin) != NULL) {
+    strcpy(buf, "*2\r\n$3\r\nGET\r\n$1\r\na");
+    Rio_writen(clientfd, buf, strlen(buf));
+    memset(buf, 0, MAXLINE);
+    strcpy(buf, "\r\n");
+    Rio_writen(clientfd, buf, strlen(buf));
+
+    Rio_writen(clientfd, buf, strlen(buf));
+    // 读取，也就是从服务器接收信息
+    Rio_readlineb(&rio, buf, MAXLINE);
+    Fputs(buf, stdout);
+    Rio_readlineb(&rio, buf, MAXLINE);
+    Fputs(buf, stdout);
+    //}
     Close(clientfd);
     exit(0);
 }
